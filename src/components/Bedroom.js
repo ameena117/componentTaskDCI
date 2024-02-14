fa73b40118ffc732ab6d7ec23afa1248b98089e3
ameena117 <ameenajadallah@gmail.com>
2024-02-14 19:41:41 +0200

    first commit

---
+++ src/components/Bedroom.js
@@ -0,0 +1,19 @@
+import '../App.css';
+
+function Bedroom({ height, width }) {
+  return (
+    <div
+      className="bedRoomComponent"
+      style={{
+        width: width,
+        height: height,
+        
+      }}
+    >
+      <h1>height: {height}</h1>
+      <h1>width: {width}</h1>
+    </div>
+  );
+}
+
+export default Bedroom;
